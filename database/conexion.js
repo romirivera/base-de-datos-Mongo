fd8b4ca2c3a6c7134219809b1375a1669ee4ddb0
Romina Rivera <rdrivera@uc.cl>
2025-02-10 19:55:05 -0300

    cambios

--- database/conexion.js
+++ database/conexion.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-
+//codigo para conectar a la base de datos
 const dbConnection = async () => {
   try {
     await mongoose.connect('mongodb://localhost:27017/back-end-users17');
--- models/usermodels.js
+++ models/usermodels.js
@@ -1,3 +1,4 @@
+const mongoose = require('mongoose');
 const { Schema, model } = mongoose;
 
 //creacion del Schema
